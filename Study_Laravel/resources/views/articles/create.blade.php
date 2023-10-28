36281bf6f468349f30e25efa3f533d38f6fafde1
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-29 03:31:59 +0900

    유효성 검사에 통과하지 못하면 폼으로 돌아온뒤 에러메시지 보여주기

--- Study_Laravel/resources/views/articles/create.blade.php
+++ Study_Laravel/resources/views/articles/create.blade.php
@@ -11,8 +11,13 @@
       <!-- <input type="hidden" name="_token" value="<?php echo csrf_token();?>"> -->
       @csrf
       <input type="text" name="body" placeholder="글을 입력해주세요" class="w-full p-3 rounded-md mb-4 focus:ring focus:ring-blue-500">
+      @error('body')
+        <p class="text-xs text-red-500 mb-3"> {{$message}} </p>
+      @enderror()
       <button type="submit" class="w-full bg-black text-black font-semibold py-2 rounded-full hover:bg-blue-600 transition duration-200">저장하기</button>
     </form>
+    <!-- {{dd($errors->all())}}  -->
+    <!-- {} 출력, all : 모든 에러 출력, any : 에러가 있는지 없는지(booean), first : 처음 에러 + 특정에러 -->
   </div>
 </body>
 </html>
