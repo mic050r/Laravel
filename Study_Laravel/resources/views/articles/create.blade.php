647cc99a9768ede0ad5b9d9f7b3a25295c21fe29
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-29 02:24:48 +0900

    자바스크립트와 CSS 불러오기

--- Study_Laravel/resources/views/articles/create.blade.php
+++ Study_Laravel/resources/views/articles/create.blade.php
@@ -1,8 +1,10 @@
 <html>
 <head>
+  @vite(['resources/css/app.css','resources/js/app.js'])
   <title></title>
 </head>
 <body>
+  <h1>글쓰기<h1>
   <form action="/articles" method="POST">
     <input type="text">
     <input type="button" value="저장하기">
