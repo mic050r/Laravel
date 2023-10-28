e304dfac8de612d14aafc91aef00af8ff20ba343
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-29 02:19:17 +0900

    글쓰기 화면 만들기

---
+++ Study_Laravel/resources/views/articles/create.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <form action="/articles" method="POST">
+    <input type="text">
+    <input type="button" value="저장하기">
+  </form>
+</body>
+</html>
